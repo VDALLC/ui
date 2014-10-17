abf5c05fee5b949e5f0c36987775502c66aa4247
Andrey Utkin <spaun@vda-team.com>
2014-10-17 11:57:26 +0700

    Fix a silly typo

--- Notification/INotificationService.php
+++ Notification/INotificationService.php
@@ -3,15 +3,15 @@ namespace Vda\Ui\Notification;
 
 interface INotificationService
 {
-    const STORAGE_REQUEST = 'request';
-    const STORAGE_SESSION = 'session';
+    const STORAGE_REQUEST    = 'request';
+    const STORAGE_SESSION    = 'session';
     const STORAGE_DATASOURCE = 'datasource';
-    const STORAGE_FALLBACK = 'fallback';
+    const STORAGE_FALLBACK   = 'fallback';
 
     /**
      * Send user notification using one or more delivery methods
      *
-     * @param string|Notificatin $notification
+     * @param string|Notification $notification
      * @param mixed $storage,...
      */
     public function push($notification, $storage);
